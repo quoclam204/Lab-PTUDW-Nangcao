25be8ed775cdbefdc778f01e8294f918e86481bc
quoclam204 <nguyenlequoclam@gmail.com>
2025-08-09 22:13:22 +0700

    Search Delete Post

--- Lab1/CodeFirstToExistingDatabase/CodeFirstToExistingDatabase/Program.cs
+++ Lab1/CodeFirstToExistingDatabase/CodeFirstToExistingDatabase/Program.cs
@@ -11,5 +11,33 @@ namespace CodeFirstToExistingDatabase
         static void Main(string[] args)
         {
         }
+
+        public static string isValidBirthday(string dateStr)
+        {
+            int count = 0;
+            for (int i = 1; i < dateStr.Length; i++)
+            {
+                if (dateStr[i] == '/')
+                {
+                    count++;
+                }
+            }
+            if (count != 2)
+            {
+                return "Ngày không hợp lệ - Sai định dạng";
+            }
+
+            // Giả lập kiểm tra đơn giản
+            if (dateStr == "29/02/2007")
+            {
+                return "Ngày không hợp lệ - Năm không nhuận";
+            }
+            if (dateStr == "10/10/2008")
+            {
+                return "Ngày không hợp lệ - Chưa đủ 18 tuổi";
+            }
+
+            return "Ngày sinh hợp lệ";
+        }
     }
 }
--- Lab1/CodeFirstToNewDatabase/CodeFirstToNewDatabase/Program.cs
+++ Lab1/CodeFirstToNewDatabase/CodeFirstToNewDatabase/Program.cs
@@ -9,17 +9,24 @@ namespace CodeFirstToNewDatabase
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             using (var db = new BloggingContext())
             {
                 //var blog = new Blog { Name = "name" };
                 //db.Blogs.Add(blog);
                 //db.SaveChanges();
 
+                db.Database.Delete();   // Drop DB CodeFirstEF_DB
+                db.Database.Create();
+
                 // Thêm mới dữ liệu cho bảng Blog
                 db.Blogs.Add(new Blog { Name = "Văn hóa" });
                 db.Blogs.Add(new Blog { Name = "Xã hội" });
                 db.Blogs.Add(new Blog { Name = "Tự nhiên" });
                 db.Blogs.Add(new Blog { Name = "Kinh tế" });
+
+                db.SaveChanges();
 
                 // Thêm mới dữ liệu cho bảng Post
                 db.Posts.Add(new Post
@@ -46,8 +53,65 @@ namespace CodeFirstToNewDatabase
                     Content = "DEF",
                     BlogId = 4
                 });
+
                 db.SaveChanges();
+
+               XuatDanhSachPost(db);
+
+                // Truy vấn các Post có BlogId tên là "Xã hội"
+                var blog = db.Blogs.FirstOrDefault(b => b.Name == "Xã hội");
+                if (blog != null)
+                {
+                    // Lấy các Post có BlogId tương ứng
+                    var posts = db.Posts.Where(p => p.BlogId == blog.BlogId).ToList();
+                    Console.WriteLine($"\nCác bài viết trong blog '{blog.Name}':");
+
+                    // Duyệt và hiển thị kết quả
+                    foreach (var post in posts)
+                    {
+                        Console.WriteLine($"\tPost Title: {post.Title}, Content: {post.Content}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Không tìm thấy blog tên 'Xã hội'");
+                }
+
+                // Xóa mục Post có PostId = 4
+                var xoaPost = db.Posts.Find(4);
+                if (xoaPost != null)
+                {
+                    db.Posts.Remove(xoaPost);
+                    db.SaveChanges();
+                    Console.WriteLine("\nĐã xóa bài viết có PostId = 4.");
+                }
+                else
+                {
+                    Console.WriteLine("\nKhông tìm thấy bài viết có PostId = 4.");
+                }
+                // Hiển thị lại danh sách các Blog và Post sau khi xóa
+                XuatDanhSachPost(db);
+
+
+
+                // Dừng chương trình và đợi người dùng nhấn Enter
+                Console.WriteLine("Nhấn Enter để thoát...");
+                Console.ReadLine();
             }
+
+        }
+
+        // Hàm xuất danh sách các post
+        static void XuatDanhSachPost(BloggingContext db)
+        {
+            var posts = db.Posts.Include(p => p.Blog).ToList();
+            Console.WriteLine("Danh sách các Post:");
+            Console.WriteLine("----------------------------------------------------------------------------------------------------");
+            foreach (var post in posts)
+            {
+                Console.WriteLine($"\tPostId: {post.PostId}, Title: {post.Title}, Content: {post.Content}, Blog: {post.Blog?.Name}");
+            }
+            Console.WriteLine("----------------------------------------------------------------------------------------------------");
         }
     }
 }
