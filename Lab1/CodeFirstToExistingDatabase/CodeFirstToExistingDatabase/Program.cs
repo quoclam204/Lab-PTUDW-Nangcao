a1f5b8518034c14b7b21e3eaa2c3475d7b9d1c21
quoclam204 <nguyenlequoclam@gmail.com>
2025-08-11 23:04:03 +0700

    feat: update EF6 model & Program.cs

--- Lab1/CodeFirstToExistingDatabase/CodeFirstToExistingDatabase/Program.cs
+++ Lab1/CodeFirstToExistingDatabase/CodeFirstToExistingDatabase/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,70 +11,23 @@ namespace CodeFirstToExistingDatabase
     {
         static void Main(string[] args)
         {
-            using (var db = new BloggingContext())
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // Khởi tạo kết nối đến cơ sở dữ liệu
+            string connectionString = @"Data Source=DESKTOP-E3V9138\SQLEXPRESS;Initial Catalog=ef_lab1;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-                //var blog = new Blog { Name = "name" };
-                //db.Blogs.Add(blog);
-                //db.SaveChanges();
-
-                db.Database.Delete();   // Drop DB CodeFirstEF_DB
-                db.Database.Create();
-
-                // Thêm mới dữ liệu cho bảng Blog
-                db.Blogs.Add(new Blog { Name = "Văn hóa" });
-                db.Blogs.Add(new Blog { Name = "Xã hội" });
-                db.Blogs.Add(new Blog { Name = "Tự nhiên" });
-                db.Blogs.Add(new Blog { Name = "Kinh tế" });
-
-                db.SaveChanges();
-
-                // Thêm mới dữ liệu cho bảng Post
-                db.Posts.Add(new Post
+                try
                 {
-                    Title = "Bóng đá Việt Nam thay huấn luyện viên",
-                    Content = "Ông Nguyễn Hữu Thắng trở thành tân\r\nhuấn luyện viên tuyển Việt Nam",
-                    BlogId = 1
-                });
-                db.Posts.Add(new Post
+                    conn.Open();
+                    Console.WriteLine("Kết nối thành công!");
+                }
+                catch (Exception ex)
                 {
-                    Title = "Tiêm phòng vắc xin bệnh dại ",
-                    Content = "Tiêm phòng ngày 25/02/2012",
-                    BlogId = 2
-                });
-                db.Posts.Add(new Post
-                {
-                    Title = "Tin tự nhiên",
-                    Content = "Tin tự nhiên",
-                    BlogId = 2
-                });
-                db.Posts.Add(new Post
-                {
-                    Title = "ABC",
-                    Content = "DEF",
-                    BlogId = 4
-                });
-
-                db.SaveChanges();
-
-                XuatDanhSachPost(db);
+                    Console.WriteLine("Kết nối thất bại: " + ex.Message);
+                }
             }
 
-        }
-
-        // Hàm xuất danh sách các post
-        static void XuatDanhSachPost(BloggingContext db)
-        {
-            // Fix for the CS1660 error
-            var posts = db.Posts.Include("Blog").ToList();
-            Console.WriteLine("Danh sách các Post:");
-            Console.WriteLine("----------------------------------------------------------------------------------------------------");
-            foreach (var post in posts)
-            {
-                Console.WriteLine($"\tPostId: {post.PostId}, Title: {post.Title}, Content: {post.Content}, Blog: {post.Blog?.Name}");
-            }
-            Console.WriteLine("----------------------------------------------------------------------------------------------------");
         }
     }
 }
---
+++ Lab1/sql_cau2.sql
@@ -0,0 +1,75 @@
+﻿-- Tạo database
+CREATE DATABASE ef_lab1;
+GO
+USE ef_lab1;
+GO
+
+-- Tạo bảng Blog
+CREATE TABLE Blog (
+    BlogId INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL
+);
+
+-- Tạo bảng Post
+CREATE TABLE Post (
+    PostId INT IDENTITY(1,1) PRIMARY KEY,
+    Title NVARCHAR(200) NOT NULL,
+    Content NVARCHAR(MAX) NULL,
+    BlogId INT NOT NULL,
+    CONSTRAINT FK_Post_Blog FOREIGN KEY (BlogId) REFERENCES Blog(BlogId)
+);
+
+-- Dữ liệu mẫu cho Blog
+INSERT INTO Blog (Name) VALUES
+(N'Văn hóa'),
+(N'Xã hội'),
+(N'Tự nhiên'),
+(N'Kinh tế');
+
+-- Dữ liệu mẫu cho Post
+INSERT INTO Post (Title, Content, BlogId) VALUES
+(N'Bóng đá Việt Nam thay huấn luyện viên', N'Ông Nguyễn Hữu Thắng trở thành tân huấn luyện viên tuyển Việt Nam', 1),
+(N'Tiêm phòng vắc xin bệnh dại', N'Tiêm phòng ngày 25/02/2012', 2),
+(N'Tin tự nhiên', N'Tin tự nhiên', 2),
+(N'ABC', N'DEF', 4);
+
+-- Tạo database
+CREATE DATABASE ef_lab1;
+GO
+USE ef_lab1;
+GO
+
+-- Tạo bảng Blog
+CREATE TABLE Blog (
+    BlogId INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL
+);
+
+-- Tạo bảng Post
+CREATE TABLE Post (
+    PostId INT IDENTITY(1,1) PRIMARY KEY,
+    Title NVARCHAR(200) NOT NULL,
+    Content NVARCHAR(MAX) NULL,
+    BlogId INT NOT NULL,
+    CONSTRAINT FK_Post_Blog FOREIGN KEY (BlogId) REFERENCES Blog(BlogId)
+);
+
+-- Dữ liệu mẫu cho Blog
+INSERT INTO Blog (Name) VALUES
+(N'Văn hóa'),
+(N'Xã hội'),
+(N'Tự nhiên'),
+(N'Kinh tế');
+
+-- Dữ liệu mẫu cho Post
+INSERT INTO Post (Title, Content, BlogId) VALUES
+(N'Bóng đá Việt Nam thay huấn luyện viên', N'Ông Nguyễn Hữu Thắng trở thành tân huấn luyện viên tuyển Việt Nam', 1),
+(N'Tiêm phòng vắc xin bệnh dại', N'Tiêm phòng ngày 25/02/2012', 2),
+(N'Tin tự nhiên', N'Tin tự nhiên', 2),
+(N'ABC', N'DEF', 4);
+
+SELECT *
+FROM Blog
+
+SELECT *
+FROM Blog
