f096d38747b2d5798c754d468d278797cdb111a8
quoclam204 <nguyenlequoclam@gmail.com>
2025-08-16 22:32:07 +0700

    Lab02

---
+++ Lab2/CodeProject/EntityTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using EntityModel; // DbContext EF nằm trong namespace này
+
+namespace EntityTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Nếu vẫn bị nhận nhầm, có thể ghi rõ tên đầy đủ:
+            // using (var db = new EntityModel.EF())
+            using (var db = new EF())
+            {
+                foreach (var b in db.Blogs.ToList())
+                {
+                    Console.WriteLine($"{b.BlogId} - {b.Name}");
+                }
+            }
+
+            Console.WriteLine("\nHoàn tất. Nhấn phím bất kỳ để thoát...");
+            Console.ReadKey();
+        }
+    }
+}
