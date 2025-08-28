55f391582505c11e161a26cbc09571e1cbfbac74
quoclam204 <nguyenlequoclam@gmail.com>
2025-08-28 23:27:27 +0700

    lab05

---
+++ Lab4/MvcBlog/MvcBlog/Models/Metadata.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcBlog.Models
+{
+    public class BlogMetadata
+    {
+        [Required(ErrorMessage = "Tên blog không được để trống")]
+        [StringLength(50)]
+        [Display(Name = "Tên")]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "Mô tả")]
+        public string Description { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Người sở hữu")]
+        public string Owner { get; set; }
+
+        [Range(1, 100)]
+        [Display(Name = "Xếp hạng")]
+        public int Rank { get; set; }
+    }
+
+    public class PostMetadata
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        [Display(Name = "Tiêu đề")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 10)]
+        [Display(Name = "Nội dung")]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Blog ID")]
+        public int BlogId { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Ngày tạo")]
+        public System.DateTime CreatedDate { get; set; }
+    }
+}
